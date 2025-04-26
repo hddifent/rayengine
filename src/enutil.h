69b4d13b906ef6984bbcd2f496d43e2106719570
hddifent <preukwork@gmail.com>
2025-04-26 15:46:06 +0700

    Added integer vector util.

---
+++ src/enutil.h
@@ -0,0 +1,25 @@
+#ifndef ENUTIL_H
+#define ENUTIL_H
+
+// Vector2, 2 components
+typedef struct Vector2Int {
+    int x;                  // Vector x component
+    int y;                  // Vector y component
+} Vector2Int;
+
+// Vector3, 3 components
+typedef struct Vector3Int {
+    int x;                  // Vector x component
+    int y;                  // Vector y component
+    int z;                  // Vector z component
+} Vector3Int;
+
+// Vector4, 4 components
+typedef struct Vector4Int {
+    int x;                  // Vector x component
+    int y;                  // Vector y component
+    int z;                  // Vector z component
+    int w;                  // Vector w component
+} Vector4Int;
+
+#endif
